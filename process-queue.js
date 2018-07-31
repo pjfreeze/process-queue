e79f7f4d92b3e7142e77f4601370f99094f8d375
Peter Freeze <pjfreeze+github@gmail.com>
2018-07-31 09:51:01 -0500

    Clean up documentation & API

--- process-queue.js
+++ process-queue.js
@@ -9,7 +9,9 @@
   const noop = function () {};
   const defaults = {
     concurrent: 5,
-    delay: 0, // Using 0 to prevent locking up the thread
+    delay: 0, // Using 0 to prevent locking up the browser thread
+    onEmpty: noop,
+    onError: noop,
     Promise: Promise, // "Native" promises
   };
 
@@ -30,49 +32,56 @@
      * @param {function} processFn - The function that will process each item
      * @param {array<*>} [queue=[]] - The queue may contain any type, items processed with processFn
      * @param {object} [options={}]
-     * @param {boolean} [options.autostart=false] - autostart on construction
-     * @param {number} [options.concurrent=5] - maximum number of concurrent consumers
-     * @param {number} [options.delay=0] - delay in milliseconds before each tick
-     * @param {function} [options.onEmpty] - callback for the queue being empty
-     * @param {function} [options.onError] - callback for an error encountered during processing
-     * @param {function} [options.Promise] - optionally specify the promise constructor used
+     * @param {boolean} [options.autostart=false] - Autostart on construction, or wait for "start"
+     * @param {number} [options.concurrent=5] - The maximum number of active/concurrent consumers
+     * @param {number} [options.delay=0] - Milliseconds to wait before each "tick"
+     * @param {function} [options.onEmpty=noop] - Called each time the queue becomes empty
+     * @param {function} [options.onError=noop] - Called for each error encountered while processing
+     * @param {function} [options.Promise=Promise] - The Promise to test processFn return values for
      */
     constructor(processFn, queue = [], options = {}) {
       this.processFn = processFn;
-      this.queue = Array.from(queue);
+      this._unprocessed = Array.from(queue);
 
-      // Number of active consumers
-      this.consumers = 0;
+      this.activeConsumerCount = 0;
       this.isRunning = false;
 
       this.options = {};
       this.options.concurrent = options.concurrent || defaults.concurrent;
       this.options.delay = options.delay || defaults.delay;
-      this.options.onEmpty = options.onEmpty || noop;
-      this.options.onError = options.onError || noop;
+      this.options.onEmpty = options.onEmpty || defaults.onEmpty;
+      this.options.onError = options.onError || defaults.onError;
       this.options.Promise = options.Promise || defaults.Promise;
 
       this.stats = {};
       this.stats.processed = 0;
       this.stats.errored = 0;
       setProperty(this.stats, 'completed', () => this.stats.processed + this.stats.errored);
-      setProperty(this.stats, 'remaining', () => this.queue.length + this.consumers);
+      setProperty(this.stats, 'remaining', () => this._unprocessed.length + this.activeConsumerCount);
       setProperty(this.stats, 'total', () => this.stats.remaining + this.stats.completed);
 
-      if (options.autostart == true) {
+      if (options.autostart) {
         this.start();
       }
+    }
+
+    get isEmpty() {
+      return this._unprocessed.length == 0;
+    }
+
+    get hasActiveConsumer() {
+      return this.activeConsumerCount > 0;
     }
 
     /**
      * Add one or more items to the end of the queue. If the queue has been
      * started and is empty, restart the queue.
      */
-    append() {
-      Array.prototype.push.apply(this.queue, Array.from(arguments));
+    append(...args) {
+      this._unprocessed.push(...args);
 
       // If there are items to process and the queue has been started, continue
-      if (this.queue.length > 0 && this.isRunning) {
+      if (!this.isEmpty && this.isRunning) {
         this.start();
       }
     }
@@ -81,11 +90,11 @@
      * Add one or more items to the beginning of the queue. If the queue has been
      * started and is empty, restart the queue.
      */
-    prepend() {
-      Array.prototype.unshift.apply(this.queue, Array.from(arguments));
+    prepend(...args) {
+      this._unprocessed.unshift(...args);
 
       // If there are items to process and the queue has been started, continue
-      if (this.queue.length > 0 && this.isRunning) {
+      if (!this.isEmpty && this.isRunning) {
         this.start();
       }
     }
@@ -98,14 +107,14 @@
       this.isRunning = true;
 
       // Don't bother evaluating the loop if there are no items that can start
-      if (this.queue.length == 0) { return; }
+      if (this.isEmpty) { return; }
 
       // The maximum number of consumers is either the maximum concurrent
       // consumers option, or if smaller, the number of items
-      const numberOfItems = this.queue.length;
-      const concurrent = this.options.concurrent - this.consumers;
+      const numberOfItems = this._unprocessed.length;
+      const concurrent = this.options.concurrent - this.activeConsumerCount;
       for (let toStart = Math.min(concurrent, numberOfItems); toStart > 0; toStart -= 1) {
-        this.consume();
+        this._consume();
       }
     }
 
@@ -118,22 +127,21 @@
     }
 
     /**
-     * @private
      * Should only be called by ProcessQueue. Consumes a single item from the queue
      * and then returns control to "tick"
      */
-    consume() {
-      if (this.isRunning && this.queue.length > 0) {
+    _consume() {
+      if (this.isRunning && !this.isEmpty) {
         // Add this consumer to the total known
-        this.consumers += 1;
+        this.activeConsumerCount += 1;
 
-        const item = this.queue.shift();
+        const item = this._unprocessed.shift();
 
         // Ensure that "tick" is only called once by wrapping it in a conditional
         const proceed = (wasError) => {
           if (!proceed.called) {
             proceed.called = true;
-            this.tick(wasError);
+            this._tick(wasError);
           }
         };
         proceed.called = false;
@@ -158,7 +166,7 @@
                 proceed(true);
               });
           } else {
-            proceed();
+            proceed(false);
           }
         };
 
@@ -167,34 +175,29 @@
     }
 
     /**
-     * @private
      * Handles determination of when to call "onEmpty" handler as well as when
      * to continue consuming the queue.
      */
-    tick(wasError) {
+    _tick(wasError) {
       if (wasError) {
         this.stats.errored += 1;
       } else {
         this.stats.processed += 1;
       }
-      this.consumers -= 1;
+      this.activeConsumerCount -= 1;
 
       // If there are no consumers and nothing in the queue, call "onEmpty"
-      if (this.queue.length == 0 && this.consumers == 0) {
+      if (this.isEmpty && !this.hasActiveConsumer) {
         this.options.onEmpty();
         return;
       }
 
-      // If the queue is currently processing, continue with the next item
-      if (this.isRunning && this.queue.length > 0) {
-        this.consume();
-      }
+      this._consume();
     }
   }
 
   // Export logic based on Scott Hamper's Cookies.js project
   // https://github.com/ScottHamper/Cookies/blob/1.2.3/src/cookies.js
-
   if (typeof define == 'function' && define.amd) {
     // AMD support
     define(function () { return ProcessQueue; });
